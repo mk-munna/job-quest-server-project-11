49a538ae21d167e345b1959c1f2b8a7fdc1bd5a3
mk-munna <mkmunnaofficial@gmail.com>
2024-05-15 23:53:06 +0600

    complete

--- index.js
+++ index.js
@@ -7,9 +7,45 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "https://jobquest-4ccdb.web.app",
+        ],
+        credentials: true,
+    })
+);
 app.use(express.json());
 app.use(cookieParser());
+
+const cookieOptions = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+
+
+// Middleware
+const logger = async (req, res, next) => {
+    console.log(`${req.method} ${req.host} ${req.url}`);
+    next();
+}
+
+
+const verifyToken = (req, res, next) => {
+    const token = req.cookies.token;
+    if (!token) {
+        return res.status(401).send({ message: 'Token is required' })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized' })
+        }
+        req.user = decoded;
+        next()
+    })
+}
 
 
 
@@ -32,6 +68,25 @@ async function run() {
         // await client.connect();
         const JobCollection = client.db('JobsDB').collection('JobCollection')
         const ApplicationCollection = client.db('ApplicationDB').collection('ApplicationCollection')
+
+// token
+        app.post("/jwt", logger, async (req, res) => {
+            const user = req.body;
+            console.log("user for token", user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+
+            res.cookie("token", token, cookieOptions).send({ success: true });
+        });
+
+        //clearing Token
+        app.post("/logout", async (req, res) => {
+            const user = req.body;
+            console.log("logging out", user);
+            res
+                .clearCookie("token", { ...cookieOptions, maxAge: 0 })
+                .send({ success: true });
+        });
+
 
         app.get('/jobs', async (req, res) => {
             const jobs = await JobCollection.find({}).toArray();
@@ -117,7 +172,7 @@ async function run() {
         })
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
